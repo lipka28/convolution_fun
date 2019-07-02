23fdc30a32c2474edc776268508690fbcb84bd13
Ladislav Lipka <lipka28@gmail.com>
2019-07-02 09:49:41 +0200

    timing of signle and multi threaded performance

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "../headers/png_loader.hpp"
 #include "../headers/compute_module.hpp"
@@ -20,17 +21,27 @@ int main(int argc, char **argv){
     cout << "Something broke on image load" << endl;
     return 2;
   }
-  
   cout << "finished loading image" << endl;
+
   cout << "creating compute modue from image" << endl;
   auto comp_module = make_unique<Compute_module>(png_image);
   cout << "finished creating compute modue from image" << endl;
-  cout << "started cpu compute" << endl;
-  comp_module->fast_cpu_edge_detection(0);
-  png_image = comp_module->get_processed_image();
-  cout << "finished cpu compute" << endl;
-  cout << "starting saving image" << endl;
 
+  cout << "started slow cpu compute" << endl;
+  auto start = chrono::high_resolution_clock::now();
+  comp_module->slow_cpu_edge_detection();
+  auto end = chrono::high_resolution_clock::now();
+  cout << "finished slow cpu compute in: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
+  
+  cout << "started fast cpu compute" << endl;
+  start = chrono::high_resolution_clock::now();
+  comp_module->fast_cpu_edge_detection(0);
+  end = chrono::high_resolution_clock::now();
+  cout << "finished fast cpu compute in: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
+  
+  png_image = comp_module->get_processed_image();
+  
+  cout << "starting saving image" << endl;
   if(Png_loader::save_to_file(png_image, argv[2]))
   {
     cout << "something broke on image save" << endl;
